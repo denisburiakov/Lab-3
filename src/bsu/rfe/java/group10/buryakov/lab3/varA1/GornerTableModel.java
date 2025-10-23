75385da3bed53c9a1a83bbfe9a51535b7fdf1a4b
Lab2 <burakovdenis36@gmail.com>
2025-10-23 10:12:58 +0300

    Добавление switch expression замена if-else на switch

--- src/bsu/rfe/java/group10/buryakov/lab3/varA1/GornerTableModel.java
+++ src/bsu/rfe/java/group10/buryakov/lab3/varA1/GornerTableModel.java
@@ -40,32 +40,34 @@ public class GornerTableModel extends AbstractTableModel {
     public Object getValueAt(int row, int col) {
         double x = from + step * row;
 
-        if (col == 0) {
-            // Первый столбец - значение X
-            return x;
-        } else if (col == 1) {
-            // Второй столбец - значение многочлена
-            Double result = 0.0;
+        switch (col) {
+            case 0:// Первый столбец - значение X
+                return x;
+            case 1:
+                // Второй столбец - значение многочлена
+                Double result = 0.0;
 
-            if (coefficients.length > 0) {
-                result = coefficients[0];
-                for (int i = 1; i < coefficients.length; i++) {
-                    result = result * x + coefficients[i];
+                if (coefficients.length > 0) {
+                    result = coefficients[0];
+                    for (int i = 1; i < coefficients.length; i++) {
+                        result = result * x + coefficients[i];
+                    }
                 }
-            }
-            return result;
-        } else {
-            // Третий столбец - больше ли нуля многочлен
-            Double result = 0.0;
+                return result;
+            case 2:
+                // Третий столбец - больше ли нуля многочлен
+                result = 0.0;
 
-            if (coefficients.length > 0) {
-                result = coefficients[0];
-                for (int i = 1; i < coefficients.length; i++) {
-                    result = result * x + coefficients[i];
+                if (coefficients.length > 0) {
+                    result = coefficients[0];
+                    for (int i = 1; i < coefficients.length; i++) {
+                        result = result * x + coefficients[i];
+                    }
                 }
-            }
-            // Возвращаем Boolean значение: true если результат > 0, иначе false
-            return result > 0;
+                // Возвращаем Boolean значение: true если результат > 0, иначе false
+                return result > 0;
+            default:
+                return "";
         }
     }
 
@@ -83,15 +85,10 @@ public class GornerTableModel extends AbstractTableModel {
     }
 
     public Class<?> getColumnClass(int col) {
-        switch (col) {
-            case 0:
-                return Double.class;
-            case 1:
-                return Double.class;
-            case 2:
-                return Boolean.class;
-            default:
-                return Object.class;
-        }
+        return switch (col) {
+            case 0, 1 -> Double.class;
+            case 2 -> Boolean.class;
+            default -> Object.class;
+        };
     }
 }
